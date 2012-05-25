43b9d97b77b7185aef57fc22fbe6d86e2e3b4e87
Ross Hadden <rosshadden@gmail.com>
2012-05-24 22:07:58 -0400

    Negation plugin.

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+S.register(/^\*/, function(){
+	this.push.apply(this, S._collection.slice());
+}, false);
+
+S.register(/!\w+/, function(selector){
+	var item = selector.substr(selector.indexOf('!') + 1).replace(/\s|\[|\]/g, '');
+
+	this.except = this.except || [];
+
+	this.except.push(item);
+
+	var i = 0;
+	while(i < this.length){
+		if(this[i].type === item){
+			this.splice(i, 1);
+
+			i -= 1;
+		}
+
+		i += 1;
+	}
+});
+
+S.register(/^\[.+\]$/, function(selector){
+	var	param = selector.replace(/\s|\[|\]/g, ''),
+		parts = param.split('=');
+
+	this.testing = 'qwer';
+
+	var i, length = S._collection.length;
+	for(i = 0; i < length; i++){
+		if(
+			parts.length === 1 && S._collection[i].object[parts[0]]
+		||	parts.length === 2 && S._collection[i].object[parts[0]] === parts[1]
+		){
+			this.push(S._collection[i]);
+			continue;
+		}
+	}
+});
+
 S.create('dog', {
 	name: 'Fred',
 	speech: 'bark',
@@ -20,25 +61,5 @@ S.create('human', {
 	speech: 'WORDS',
 	speak: function(what){
 		console.log(this.name, 'says', what, 'with his', this.speech);
-	}
-});
-
-S.register('*', function(){
-	this.push.apply(this, S._collection.slice());
-}, true);
-
-S.register(/^\[.+\]$/, function(selector){
-	var	param = selector.replace(/\s|\[|\]/g, ''),
-		parts = param.split('=');
-
-	var i, length = S._collection.length;
-	for(i = 0; i < length; i++){
-		if(
-			parts.length === 1 && S._collection[i].object[parts[0]]
-		||	parts.length === 2 && S._collection[i].object[parts[0]] === parts[1]
-		){
-			this.push(S._collection[i]);
-			continue;
-		}
 	}
 });
