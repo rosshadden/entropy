c5134f13b5047a26d196333ba75691c09476f42b
Ross Hadden <rosshadden@gmail.com>
2013-04-28 18:00:22 -0400

    Improved formatting.

--- playground/js/main.js
+++ playground/js/main.js
@@ -1,24 +1,22 @@
 $('#console').console({
-	promptLabel: 'λ> ',
-	commandValidate:function(line){
+	promptLabel: 'λ ',
+	autofocus: true,
+	animateScroll: true,
+	promptHistory: true,
+	commandValidate: function(line){
 		if (line === "") return false;
 		else return true;
 	},
-	commandHandle:function(line){
+	commandHandle: function(line){
 		return [{
-			msg:"=> [12,42]",
-			className:"jquery-console-message-value"
+			msg: "=> [12,42]",
+			className: "jquery-console-message-value"
 		},{
-			msg:":: [a]",
-			className:"jquery-console-message-type"
+			msg: ":: [a]",
+			className: "jquery-console-message-type"
 		}];
 	},
-	autofocus:true,
-	animateScroll:true,
-	promptHistory:true,
-	charInsertTrigger:function(keycode,line){
-		// Let you type until you press a-z
-		// Never allow zero.
-		return !line.match(/[a-z]+/) && keycode != '0'.charCodeAt(0);
+	charInsertTrigger: function(keycode,line){
+		return true;
 	}
 });
