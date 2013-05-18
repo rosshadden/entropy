ea6e370ada0d118a0da53183901130ace0463947
Ross Hadden <rosshadden@gmail.com>
2013-05-18 11:35:35 -0400

    Added set.map.

--- entropy.js
+++ entropy.js
@@ -919,6 +919,38 @@
 				});
 			},
 
+			map: function(callback, thisArg){
+				var T, A, k;
+
+				if (this == null) {
+					throw new TypeError(" this is null or not defined");
+				}
+
+				var O = Object(this);
+				var len = O.length >>> 0;
+
+				if (typeof callback !== "function") {
+					throw new TypeError(callback + " is not a function");
+				}
+				if (thisArg) {
+					T = thisArg;
+				}
+
+				A = new Set(len);
+				k = 0;
+
+				while (k < len) {
+					var kValue, mappedValue;
+					if (k in O) {
+						kValue = O[k];
+						mappedValue = callback.call(T, kValue, k, O);
+						A[k] = mappedValue;
+					}
+					k++;
+				}
+				return A;
+			},
+
 			union: function(){
 				var set = new Set();
 				return set.concat.apply(set, [this].concat(arguments));
