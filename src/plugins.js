9a43b133f69fa132e4d81af81eb00910698746fb
Ross Hadden <rosshadden@gmail.com>
2013-10-24 15:29:27 -0400

    Added class plugin.

--- src/plugins.js
+++ src/plugins.js
@@ -8,16 +8,33 @@
 
 	entropy.register("id", {
 		check: /^#([\w\-_]+)$/,
-		filter(element, e, $id) {
-			return element.data.id === $id;
-		}
 		hooks: {
 			"element.create"(element, value, ...args) {
 				args.forEach((arg) => {
+					element.data.id = "";
 					let match = (""+arg).match(this.check);
 					if (match) element.data.id = match[1];
 				});
 			}
+		},
+		filter(element, e, $id) {
+			return element.data.id === $id;
+		}
+	});
+
+	entropy.register("class", {
+		check: /^\.([\w\-_]+)$/,
+		hooks: {
+			"element.create"(element, value, ...args) {
+				args.forEach((arg) => {
+					element.data.classes = [];
+					let match = (""+arg).match(this.check);
+					if (match) element.data.classes.push(match[1]);
+				});
+			}
+		},
+		filter(element, e, $class) {
+			return !!~element.data.classes.indexOf($class);
 		}
 	});
 })();
